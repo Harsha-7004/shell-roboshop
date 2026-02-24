420c47486b92e6dc78fcde1b1b4c30bf9a253e38
Harsha Reddy <harshavardhan7004@gmail.com>
2026-02-24 19:50:03 +0530

    changed something

--- roboshop.sh
+++ roboshop.sh
@@ -7,13 +7,13 @@ for instance in $@
 do 
     instance_id=$(aws ec2 run-instances \
     --image-id $AMI_ID \
-    --instance-type t3.micro \
+    --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance }]" \
     --query 'Instances[0].InstanceId' \
     --output text)
 
-   if [ $instance_id == "frontend" ]; then 
+   if [ $instance == "frontend" ]; then 
         IP=$(
         aws ec2 describe-instances \
         --instance-ids $instance_id \ 
